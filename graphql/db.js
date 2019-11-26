276ee3da110ee2c4b95273e9af329f22831486dd
mingyumkim93 <mingyumkim93@gmail.com>
2019-11-26 13:30:29 +0200

    creating mutation for adding a movie

--- graphql/db.js
+++ graphql/db.js
@@ -1,29 +1,50 @@
-export const people = [
+const movies = [
     {
-        id: "0",
-        name: "Nicolas",
-        age: 18,
-        gender: "male"
-},
-{       id: "1",
-        name: "Mingyum",
-        age: 26,
-        gender: "male"
-},
-{       id: "2",
-        name: "Seppo",
-        age: 45,
-        gender: "male"
-},
-{       id: "3",
-        name: "Mom",
-        age: 55,
-        gender: "female"
-}
-
+        id: 0,
+        name: "Train to Busan",
+        score: 5
+    },
+    {
+        id: 1,
+        name: "Parasite",
+        score: 54
+    },
+    {
+        id: 2,
+        name: "Terminator",
+        score: 15
+    },
+    {
+        id: 3,
+        name: "Avengers",
+        score: 50
+    }
 ]
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => String(id) ===person.id);
-    return  filteredPeople[0]
+    const filteredMovies = movies.filter(movie => id === movie.id);
+    return  filteredMovies[0]
+}
+
+export const deleteMoive = () => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if(movies.length > cleanedMovies.length){
+        movie = cleanedMovies;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+export const addMoive = (name, score) => {
+    const newMovie= {
+        id: movies.length + 1,
+        name,
+        score
+    }
+    movies.push(newMovie);
+    return newMovie;
 }
--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,9 +1,13 @@
-import { people, getById} from "./db"
+import { getMovies, getById, addMoive} from "./db"
 
 const resolvers = {
     Query:{
-        people: () => people,
-        person: (_, {id}) => getById(id)
+        movies: () => getMovies(),
+        movie: (_, {id}) => getById(id)
+    },
+
+    Mutation:{
+        addMovie:(_,{name, score})=> addMoive(name, score)
     }
 }
 
