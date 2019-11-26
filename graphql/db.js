33695a414e817653ee3616abf12c12e9925a722a
mingyumkim93 <mingyumkim93@gmail.com>
2019-11-26 13:48:00 +0200

    creating delete mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,4 +1,4 @@
-const movies = [
+let movies = [
     {
         id: 0,
         name: "Train to Busan",
@@ -28,10 +28,10 @@ export const getById = id => {
     return  filteredMovies[0]
 }
 
-export const deleteMoive = () => {
+export const deleteMovie = id => {
     const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length){
-        movie = cleanedMovies;
+        movies = cleanedMovies;
         return true;
     }
     else{
@@ -39,9 +39,9 @@ export const deleteMoive = () => {
     }
 }
 
-export const addMoive = (name, score) => {
+export const addMovie = (name, score) => {
     const newMovie= {
-        id: movies.length + 1,
+        id: movies.length,
         name,
         score
     }
