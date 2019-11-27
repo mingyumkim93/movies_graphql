4180ce2e6ef9b904c4c339d334d67e7e332aefea
mingyumkim93 <mingyumkim93@gmail.com>
2019-11-27 16:03:14 +0200

    updated features

--- graphql/db.js
+++ graphql/db.js
@@ -5,43 +5,41 @@ const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`
 
 export const getMovies = async (limit, rating) => {
-    
-    const{
-        data: {
-            data: {movies}
-        }
+    const {
+      data: {
+        data: { movies }
+      }
     } = await axios(LIST_MOVIES_URL, {
-        params:{
-            limit,
-            minimum_rating:rating
-        }
+      params: {
+        limit,
+        minimum_rating: rating
+      }
     });
     return movies;
-};
-
-export const getMovie = async id => {
-    const{
-        data:{
-            data:{movie}
-        }
-    } = await axios(MOVIE_DETAILS_URL,{
-        params: {
-            movie_id : id
-        }
+  };
+  
+  export const getMovie = async id => {
+    const {
+      data: {
+        data: { movie }
+      }
+    } = await axios(MOVIE_DETAILS_URL, {
+      params: {
+        movie_id: id
+      }
     });
     return movie;
-}
-
-export const getSuggestions = async id => {
+  };
+  
+  export const getSuggestions = async id => {
     const {
-        data: {
-            data: {movies}
-        }
-    } = await axios(MOVIE_SUGGESTIONS_URL,{
-        params:{
-            movie_id: id
-        }
+      data: {
+        data: { movies }
+      }
+    } = await axios(MOVIE_SUGGESTIONS_URL, {
+      params: {
+        movie_id: id
+      }
     });
     return movies;
-}
-    
+  };
