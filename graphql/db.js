27f909e780776c4678cd9703db8264091f5666a9
mingyumkim93 <mingyumkim93@gmail.com>
2019-11-28 10:48:20 +0200

    modification for deployment

--- graphql/db.js
+++ graphql/db.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+import axios from "axios";
 const BASE_URL = "https://yts.am/api/v2/";
 const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`
